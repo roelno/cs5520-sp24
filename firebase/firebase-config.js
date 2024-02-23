b5fff105acbe9b2ae2326abe68b4d3b544029fd6
zelda <lichtseeker@gmail.com>
2024-02-22 18:08:07 -0800

    Firebase config with env

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -4,16 +4,18 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
 
+import {apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId} from "@env";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDcRHnpz2v_D3jS6sWxIpDCP8cikv6OlZ4",
-  authDomain: "db-demo-99120.firebaseapp.com",
-  projectId: "db-demo-99120",
-  storageBucket: "db-demo-99120.appspot.com",
-  messagingSenderId: "167662265223",
-  appId: "1:167662265223:web:44c924edef224442792922",
-  measurementId: "G-SW90GLWK7W"
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
 };
 
 // Initialize Firebase
